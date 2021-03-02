821c3ed2be9330f65b7ab5946c377340ba9a6d05
tonyjo <tjoseph.ubc@gmail.com>
2021-03-01 21:16:57 -0800

    updated

--- src/HMC.py
+++ src/HMC.py
@@ -189,14 +189,75 @@ def traverse(G, node, visit={}, path=[], include_v=True):
 
 #-------------------------------------HMC---------------------------------------
 # Global vars
-DEBUG = False # Set to true to see intermediate outputs for debugging purposes
-def grad_UX(X_):
-    pass
+DEBUG = True # Set to true to see intermediate outputs for debugging purposes
+def grad_UX(X_, Y_, X, Y):
+    Ex = 0.0
+    Ey = 0.0
+    for x in X_.keys():
+        Ex = Ex + torch.log(X_[x])
+    for y in Y_.keys():
+        Ey = Ey + torch.log(Y_[y])
+    Eu = -1.0*(Ex + Ey)
 
-def leapFrog(X_, R_0, T, eps):
-    pass
+    # Compute grads
+    Eu.backward()
 
-def HMC(graph, S, eps):
+    # Collect grads
+    dU = {}
+    for x in X_.keys():
+        dU[x] = X_[x].grad
+    for y in Y_.keys():
+        dU[y] = Y_[y].grad
+
+    return dU
+
+def leapFrog(X, Y, X_, Y_, R_0, T, eps):
+    gradU = grad_UX(X_=X_, Y_=Y_, X=X, Y=Y)
+    if DEBUG:
+        print("Gradients: ", gradU)
+
+    R_12 = {}
+    for du in gradU.keys():
+        R_12[du] = R_0[du] - (0.5) * eps * gradU[du]
+
+    for t in range(T):
+        for x in X_.keys():
+            X_[x] = X_[x] + (eps * R_12[x])
+        dU_t = grad_UX(X_=X_, Y_=Y_, X=X, Y=Y)
+        for x in X_.keys():
+            R_12[x] = R_12[x] - (eps * dU_t[x])
+
+    X_T = {}
+    for x in X_.keys():
+        X_T[x] = X_[x] + (eps * R_12[x])
+    R_T = {}
+    dU_t = grad_UX(X_=X_, Y_=Y_, X=X, Y=Y)
+    for x in X_.keys():
+        R_T[x] = R_12[x] - (eps * dU_t[x])
+
+    return X_T, R_T
+
+def H(X_, Y_, X, Y, R, M):
+    # Compute U
+    Ex = 0.0
+    Ey = 0.0
+    for x in X_.keys():
+        Ex = Ex + torch.log(X_[x])
+    for y in Y_.keys():
+        Ey = Ey + torch.log(Y_[y])
+    U = -1.0*(Ex + Ey)
+    # Compute K
+    K = 0.0
+    for r in R.keys():
+        K += R[r]*torch.inverse(M[r])*R[r]
+    K = k/2.0
+
+    H = U + K
+
+    return H
+
+
+def HMC(graph, S, T, eps):
     D, G, E = graph[0], graph[1], graph[2]
 
     # Compiled graph
@@ -223,6 +284,7 @@ def HMC(graph, S, eps):
     if DEBUG:
         print('Observed Vars: ', O)
         print('Latent Vars: ', X)
+        print('\n')
 
     X_0 = {}
     for lvar in X:
@@ -237,13 +299,48 @@ def HMC(graph, S, eps):
         if DEBUG:
             print('Evaluated reverse graph path: ', path)
             print('Evaluated graph output: ', output)
-        X_0[lvar] = output[-1]
+        output_ = output[-1]
+        output_ = torch.tensor(output_, requires_grad=True)
+        X_0[lvar] = output_
     # import pdb; pdb.set_trace()
     if DEBUG:
+        print('\n')
         print('Evaluted Latent Vars: ', X_0)
         print('\n')
 
-    return None
+    Y_0 = {}
+    for yvar in Y.keys():
+        output_ = Y[yvar]
+        if isinstance(output_, int):
+            output_ = [float(output_)]
+        output_ = torch.tensor(output_, requires_grad=True)
+        Y_0[yvar] = output_
+    # import pdb; pdb.set_trace()
+    if DEBUG:
+        print('\n')
+        print('Evaluted Observed Vars: ', Y_0)
+        print('\n')
+
+    # Compute variance
+    M = {}
+    for lvar in X:
+
+    R_0 = {}
+    uniform_dist = distributions.uniform.Uniform(low=0.0, high=1.0)
+    all_outputs = []
+    for i in range(S):
+        X_T, R_T = leapFrog(X=X, Y=Y, X_=X_0, Y_=Y_0, R_0=R, T=T, eps=eps)
+        u = (uniform_dist.sample()).item()
+        accept = torch.exp((-1.0 * H(X_, Y_, X, Y, R, M)) + H(X_, Y_, X, Y, R, M))
+        if DEBUG:
+            print('\n')
+            print('Accept: ', accept)
+        if u < accept:
+            X_ =  X_T
+        all_outputs.append([X_])
+
+    return all_outputs
+
 
 #------------------------------MAIN--------------------------------------------
 if __name__ == '__main__':
@@ -253,7 +350,7 @@ if __name__ == '__main__':
 
     # run_probabilistic_tests()
 
-    for i in range(3,4):
+    for i in range(1,2):
         ## Note: this path should be with respect to the daphne path!
         # ast = daphne(['graph', '-i', f'{daphne_path}/src/programs/{i}.daphne'])
         # ast_path = f'./jsons/graphs/final/{i}.json'
@@ -272,7 +369,7 @@ if __name__ == '__main__':
             # print("Evaluation Output: \n", output)
             # print("\n")
 
-            output = HMC(graph=ast, S=1)
+            output = HMC(graph=ast, S=1, T=1, eps=0.001)
             print("Gibbs Sampling with Metropolis-Hastings Updates: ")
             print("Evaluation Output: \n", output)
             print("\n")
